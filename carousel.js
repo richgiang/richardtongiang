ee2ae8045096de33cb3283e3c83c8b75511ccad0
richgiang <77689249+richgiang@users.noreply.github.com>
2022-05-19 18:26:05 -0400

    home page and carousel

---
+++ carousel.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const carouselButtons = document.querySelectorAll(".carousel-btn");
+const carouselButtonLeft = document.querySelector(".btn-left");
+const carouselButtonRight = document.querySelector(".btn-right");
+const carouselImage1 = document.querySelector(".carousel-image-1");
+const carouselImage2 = document.querySelector(".carousel-image-2");
+const carouselImage3 = document.querySelector(".carousel-image-3");
+const numberOfImages = document.querySelectorAll(".carousel-image").length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtonLeft.addEventListener("click", slideLeft);
+carouselButtonRight.addEventListener("click", slideRight);
+
+function slideLeft() {
+  if (imageIndex !== 1) {
+    imageIndex--;
+    translateX += 100;
+    carouselImage1.style.transform = `translateX(${translateX}%)`;
+    carouselImage2.style.transform = `translateX(${translateX}%)`;
+    carouselImage3.style.transform = `translateX(${translateX}%)`;
+  }
+}
+
+function slideRight() {
+  if (imageIndex !== numberOfImages) {
+    imageIndex++;
+    translateX -= 100;
+    carouselImage1.style.transform = `translateX(${translateX}%)`;
+    carouselImage2.style.transform = `translateX(${translateX}%)`;
+    carouselImage3.style.transform = `translateX(${translateX}%)`;
+  }
+}
