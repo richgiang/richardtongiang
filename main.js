c56c131c5a6a289ac8e5d20c448e503529b5e56f
richgiang <77689249+richgiang@users.noreply.github.com>
2022-07-20 10:11:28 -0400

    revived code

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ changePickupStoreMenu();
 
 function assignSlideoutToTrigger(trigger, slideoutSelector){
     trigger.addEventListener("mouseover", function(){
-        $(slideoutSelector).css("left", "-20%");
-        $('.mask').fadeIn();
+        $(slideoutSelector).css("left", "-25%");
     } );
 }
 
 function addHidingAbilityToSlideout(listOfSlideouts){
-    $(document).on('click', ".mask", function(el,i){
+    $(document).on('mouseout', ".dish", function(el,i){
         for (var i=0; i < listOfSlideouts.length; i++){
             $(listOfSlideouts[i]).css("left", "-50%");
         }
-        $('.mask').fadeOut();
     });
 
 }
@@ -23,26 +21,29 @@ function changePickupStoreMenu(){
     var toggleSlideLeft;
     var body = $('body'),
     mask = $('<div class="mask"></div>');
-    $('body').append(mask);
 
         // ICON/TRIGGER
-        var design1 = document.querySelector("#design1"),
-        design2 = document.querySelector("#design2"),
-        design3 = document.querySelector("#design3");
+        var dish1 = document.querySelector("#dish1"),
+        dish2 = document.querySelector("#dish5"),
+        dish3 = document.querySelector("#dish3"),
+        dish4 = document.querySelector("#dish4");
         
         // SLIDEOUTs
-        var carmen =  "#carmen",
-        crash = "#crash",
-        kroger = "#kroger";
+        var designs =  "#designs",
+        media = "#media",
+        ideas = "#ideas",
+        aboutme = "#about-me";
      
     
         /* slide menu right */
-        assignSlideoutToTrigger(design1, carmen);
-        assignSlideoutToTrigger(design2, crash);
-        assignSlideoutToTrigger(design3, kroger);
+        assignSlideoutToTrigger(dish1, designs);
+        assignSlideoutToTrigger(dish2, media);
+        assignSlideoutToTrigger(dish3, ideas);
+        assignSlideoutToTrigger(dish4, aboutme);
+
     
     
-        var allSlideouts = [carmen, crash, kroger]
+        var allSlideouts = [designs, media, ideas, aboutme]
 
     addHidingAbilityToSlideout(allSlideouts)
     /* hide active menu if close menu button is clicked */
