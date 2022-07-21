0905098cc25e295b5d4272765cf013df3176c0f8
richgiang <77689249+richgiang@users.noreply.github.com>
2022-07-21 13:13:31 -0400

    IDEO site updates

--- main.js
+++ main.js
@@ -24,26 +24,29 @@ function changePickupStoreMenu(){
 
         // ICON/TRIGGER
         var dish1 = document.querySelector("#dish1"),
-        dish2 = document.querySelector("#dish5"),
+        dish2 = document.querySelector("#dish2"),
         dish3 = document.querySelector("#dish3"),
-        dish4 = document.querySelector("#dish4");
+        dish4 = document.querySelector("#dish4"),
+        dish5 = document.querySelector("#dish5");
         
         // SLIDEOUTs
-        var designs =  "#designs",
-        media = "#media",
-        ideas = "#ideas",
-        aboutme = "#about-me";
+        var whyIdeo =  "#why-ideo",
+        whyStorytelling = "#why-storytelling",
+        aboutMe = "#about-me",
+        relevantWork = "#work",
+        demonstration = "#demonstration";
      
     
         /* slide menu right */
-        assignSlideoutToTrigger(dish1, designs);
-        assignSlideoutToTrigger(dish2, media);
-        assignSlideoutToTrigger(dish3, ideas);
-        assignSlideoutToTrigger(dish4, aboutme);
+        assignSlideoutToTrigger(dish1, whyIdeo);
+        assignSlideoutToTrigger(dish2, whyStorytelling);
+        assignSlideoutToTrigger(dish3, aboutMe);
+        assignSlideoutToTrigger(dish4, relevantWork);
+        assignSlideoutToTrigger(dish5, demonstration);
 
     
     
-        var allSlideouts = [designs, media, ideas, aboutme]
+        var allSlideouts = [whyIdeo, whyStorytelling, aboutMe, relevantWork, demonstration]
 
     addHidingAbilityToSlideout(allSlideouts)
     /* hide active menu if close menu button is clicked */
